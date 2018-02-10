59e1061a44cf6abb1b5de8fe6e1778a701904632
ABDELCHAG <abdellah.chagdani@gmail.com>
2018-02-10 12:07:42 +0100

    First commit

---
+++ src/main/java/org/abdel/petitsProgrammes/App.java
@@ -0,0 +1,13 @@
+package org.abdel.petitsProgrammes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
